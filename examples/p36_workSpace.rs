929cd12302e4d303e80178463a7c180ac83923a4
Qfish <506175003@qq.com>
2026-09-02 16:22:10 +0800

    aa

--- examples/p36_workSpace.rs
+++ examples/p36_workSpace.rs
@@ -1,3 +1,41 @@
 fn main() {
     println!("Example p0: Samples of Rust");
+
+    /*
+     * 工作空间
+     * 1. 工作空间是一个目录，用于存储项目的所有文件
+     * 2. 工作空间可以包含多个项目
+     * 3. 工作空间可以包含多个项目，每个项目可以包含多个 crate
+     * #############################################################
+     * 工作空间的目录结构如下：
+     * ```
+     * work_space/
+     *     ├── crate1/
+     *     ├── crate2/
+     *     ├── crate3/
+     *     ├── ...
+     *     ├── Cargo.toml
+     *     ├── Cargo.lock
+     *     ├── README.md
+     *     ├── ...
+     * ```
+     *
+     * Cargo.toml
+     *
+     * [workspace]
+     *
+     * members = [
+     *     "crate1",
+     *     "crate2",
+     *     "crate3",
+     *     ...
+     * ]
+     * #############################################################
+     * 如果 create1 依赖 crate2，
+     * create1 的cargo.toml文件中需要添加如下内容：
+     * ```
+     * [dependencies]
+     * crate2 = { path = "../crate2" }
+     * ```
+     */
 }
--- examples/p37_box.rs
+++ examples/p37_box.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Example p0: Samples of Rust");
+    println!("Example p37: box");
+
+    let mut b = Box::new(5);
+    *b = 10;
+    println!("b = {}", b);
+
+    test01();
+}
+
+use List::Cons;
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn test01() {
+    let list = List::Cons(
+        1,
+        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    );
+
+    if let Cons(x, y) = list {
+        println!("{:?}", x);
+        println!("{:?}", y); // Box<List>
+    }
 }
