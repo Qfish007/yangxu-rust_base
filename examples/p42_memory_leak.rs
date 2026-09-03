9242589a6ce8e2a53deb575d5ea49b7e0c5118ac
Qfish <506175003@qq.com>
2026-09-03 13:40:12 +0800

    b

--- examples/p42_memory_leak.rs
+++ examples/p42_memory_leak.rs
@@ -6,7 +6,7 @@ use List::{Cons, Nil};
 // ==============================================
 #[derive(Debug)]
 enum List {
-    Cons(i32, RefCell<Rc<List>>), // RefCell<Rc<List>> 目的是为了让第二个元素可以修改
+    Cons(i32, RefCell<Rc<List>>), // RefCell  目的是为了让第二个元素可以修改,RC 是为了可以拥有多个所有者
     Nil,
 }
 
@@ -32,18 +32,19 @@ impl List {
 struct Node {
     value: i32,
     children: RefCell<Vec<Rc<Node>>>,
-    parent: RefCell<Weak<Node>>,
+    parent: RefCell<Weak<Node>>, // RefCell<Weak<Node>> 目的是为了让parent可以修改
 }
 
 fn main() {
-    test01();
+    println!("Example p42: circleAndLeak");
+    // test01();
     test02();
+    test03();
 }
 
+#[allow(unused)]
 fn test01() {
     println!("--------------test01------------");
-
-    println!("Example p42: circleAndLeak");
 
     let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
     println!("a initial rc count = {}", Rc::strong_count(&a));
@@ -64,6 +65,7 @@ fn test01() {
     println!("a first item = {:?}", a.first());
 }
 
+#[allow(unused)]
 fn test02() {
     println!("--------------test02------------");
 
@@ -79,6 +81,26 @@ fn test02() {
         parent: RefCell::new(Weak::new()),
     });
 
+    // let x = Rc::downgrade(&branch);
+
     *leaf.parent.borrow_mut() = Rc::downgrade(&branch); // 将leaf的parent指向branch，但是是Weak类型
     println!("2.leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+}
+
+#[allow(unused)]
+fn test03() {
+    println!("--------------test03------------");
+
+    // Rc<RefCell<T>>：多人共享一份数据，谁都能改内容
+    /// 典型场景 共享可变状态（如 GUI/树节点数据）
+    let a = Rc::new(RefCell::new(5));
+    let b = Rc::clone(&a); // a 和 b 指向同一个 RefCell
+    *b.borrow_mut() += 10;
+    println!("{}", b.borrow()); // 15 —— a 的修改 b 也能看到
+
+    /// RefCell<Rc<T>>：一个人拿着指针，能换指针指向谁
+    /// 典型场景 链表/图结构里改"下一个节点指向谁"
+    let m = RefCell::new(Rc::new(5));
+    *m.borrow_mut() = Rc::new(6); // Rc::new(5) 被替换为 Rc::new(6)
+    println!("m = {:?}", m); // Rc::new(6)` `
 }
